ffb6040730447b1a180008cf22f6f42a9b5992fd
OpenShift guest <rylan.collins@gmail.com>
2014-06-25 16:53:10 -0400

    Added more unit tests

---
+++ test/argsSpec.js
@@ -0,0 +1,116 @@
+var expect = require('chai').expect,
+    ruby = require('../index');
+    
+describe('passing arguments', function() {
+  ruby.require('./test/test.rb');
+  var ArgsInspector = ruby.getClass('ArgsInspector');
+  
+  it('should pass the correct number of arguments', function() {
+    var a = new ArgsInspector();
+    
+    expect(a.arg_count()).to.equal(0);
+    expect(a.arg_count('a')).to.equal(1);
+    expect(a.arg_count('a', 2)).to.equal(2);
+    expect(a.arg_count('a', 2, 'c')).to.equal(3);
+    expect(a.arg_count('a', 2, 'c', 4)).to.equal(4);
+  });
+  
+  it('should handle wrong number of arguments', function() {
+    var a = new ArgsInspector();
+    
+    var fn = function() { return a.get_type(); };
+    expect(fn).to.throw(Error);
+    
+    fn = function() { return a.get_type(1, 2); };
+    expect(fn).to.throw(Error);
+  });
+  
+  describe('undefined', function() {
+    it('should convert to nil', function() {
+      var a = new ArgsInspector();
+      var val = undefined;
+      
+      expect(a.get_type(val)).to.equal('NilClass');
+      expect(a.check_nil(val)).to.be.true;
+    });
+  });
+  
+  describe('null', function() {
+    it('should convert to nil', function() {
+      var a = new ArgsInspector();
+      var val = null;
+      
+      expect(a.get_type(val)).to.equal('NilClass');
+      expect(a.check_nil(val)).to.be.true;
+    });
+  });
+  
+  describe('true', function() {
+    it('should convert to true', function() {
+      var a = new ArgsInspector();
+      var val = true;
+      expect(a.get_type(val)).to.equal('TrueClass');
+      expect(a.check_true(val)).to.be.true;
+    });
+  });
+  
+  describe('false', function() {
+    it('should convert to false', function() {
+      var a = new ArgsInspector();
+      var val = false;
+      
+      expect(a.get_type(val)).to.equal('FalseClass');
+      expect(a.check_false(val)).to.be.true;
+    });
+  });
+  
+  describe('string', function() {
+    it('should convert to a ruby string', function() {
+      var a = new ArgsInspector();
+      var val = 'hello';
+      
+      expect(a.get_type(val)).to.equal('String');
+      expect(a.check_string(val)).to.be.true;
+    });
+  });
+  
+  describe('array', function() {
+    it('should convert to ruby array', function() {
+      var a = new ArgsInspector();
+      var val = [4, 3, 9];
+      
+      expect(a.get_type(val)).to.equal('Array');
+      expect(a.check_array(val)).to.be.true;
+    });
+  });
+  
+  describe('int32', function() {
+    it('should convert to fixnum', function() {
+      var a = new ArgsInspector();
+      var val = -49895;
+      
+      expect(a.get_type(val)).to.equal('Fixnum');
+      expect(a.check_int32(val)).to.be.true;
+    });
+  });
+  
+  describe('uint32', function() {
+    it('should convert to fixnum', function() {
+      var a = new ArgsInspector();
+      var val = 49895;
+      
+      expect(a.get_type(val)).to.equal('Fixnum');
+      expect(a.check_uint32(val)).to.be.true;
+    });
+  });
+  
+  describe('number', function() {
+    it('should convert to float', function() {
+      var a = new ArgsInspector();
+      var val = 4.359;
+      
+      expect(a.get_type(val)).to.equal('Float');
+      expect(a.check_number(val)).to.be.true;
+    });
+  });
+});
--- test/test.rb
+++ test/test.rb
@@ -23,3 +23,36 @@ class Returner
     return [4, 3, 9]
   end
 end
+
+class ArgsInspector
+  def arg_count (*args)
+    return args.length
+  end
+  def get_type (arg)
+    return arg.class.to_s
+  end
+  def check_nil (arg)
+    return arg == nil
+  end
+  def check_true (arg)
+    return arg == true
+  end
+  def check_false (arg)
+    return arg == false
+  end
+  def check_string (arg)
+    return arg == "hello"
+  end
+  def check_array (arg)
+    return arg == [4, 3, 9]
+  end
+  def check_int32 (arg)
+    return arg == -49895
+  end
+  def check_uint32 (arg)
+    return arg == 49895
+  end
+  def check_number (arg)
+    return arg == 4.359
+  end
+end
